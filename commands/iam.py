44295f10f17e3267289fa561c9c1b7ad5979d433
FelixRewer <39186283+FelixRewer@users.noreply.github.com>
2019-02-26 21:58:30 +0100

    Deleting roles

--- commands/iam.py
+++ commands/iam.py
@@ -11,7 +11,11 @@ async def main(client, message, params):
     role = ''.join(params)
     if role.lower() in [e.lower() for e in roles]:
         role = get(client.get_guild(531445761733296130).roles, name=role[0].upper()+role[1:].lower())
-        await message.author.add_roles(role)
-        await message.channel.send(content=message.author.mention + ' Rolle ' + role.name + ' hinzugefügt.')
+        if role in message.author.roles:
+            await message.author.add_roles(role)
+            await message.channel.send(content=message.author.mention + ' Rolle ' + role.name + ' hinzugefügt.')
+        else:
+            await message.author.remove_roles(role)
+            await message.channel.send(centent=message.author.mention + ' Rolle ' + role.name + ' entfernt.')
     else:
         await message.channel.send(content='Please use one of the following roles: ```\n' + '\n'.join([e for e in roles if e != 'nsfw']) + ' ```')
