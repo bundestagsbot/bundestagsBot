390052417e1ad68ad0eb706c5b74ccd819c7e304
Yannick <test.github@n.com>
2019-08-05 20:01:46 +0200

    upsi

--- commands/iam.py
+++ commands/iam.py
@@ -9,12 +9,14 @@ roles = ['Liberal', 'Konservativ', 'Sozialdemokratisch', 'Sozialistisch', 'Natio
          'Wirtschaftsliberal', 'Grün', 'Podcast', 'Patriotisch', "Finanzen", "Außenpolitik", "Justiz", "Militär",
          "Familie & Jugend", "Verkehr & Infrastruktur", "Bildung & Forschung", "Innenpolitik", "Wirtschaft",
          "Arbeit & Soziales", "Ernährung & Landwirtschaft", "Gesundheit", "Umwelt & Naturschutz", "Entwicklungshilfe"]
+lower_roles = [e.lower() for e in roles]
 
 
 async def main(client, message, params):
-    role = ' '.join(params).strip()
-    if role.lower() in [e.lower() for e in roles]:
-        role = get(client.get_guild(531445761733296130).roles, name=role)
+    want_role = ' '.join(params).strip().lower()
+    if want_role in lower_roles:
+        assign_role = roles[lower_roles.index(want_role)]
+        role = get(client.get_guild(531445761733296130).roles, name=assign_role)
         if role not in message.author.roles:
             await message.author.add_roles(role)
             await message.channel.send(content=message.author.mention + ' Rolle ' + role.name + ' hinzugefügt.')
