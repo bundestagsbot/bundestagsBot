6943a79496f3306d47ec251aafa476b70224e99b
Yannick <gross.fighterwar1@gmail.com>
2019-08-16 22:26:39 +0200

    safer workflow. handling invalid config

--- others/message_conditions.py
+++ others/message_conditions.py
@@ -2,18 +2,19 @@ from bt_utils.config import cfg
 
 
 async def check_message(client, message):
-    if cfg.options["bot_api_check"]:
-        if message.channel.id == cfg.options["bot_api_channel"]:  # To check if bot is still running (needed for API)
+    if cfg.options.get("bot_api_check", False):
+        if message.channel.id == cfg.options.get("bot_api_channel", 0):  # To check if bot is still running (needed for API)
             await message.delete()
+            return False
 
     if message.author == client.user:
         return False
 
-    if cfg.options["use_blacklist"]:
-        if str(message.author.id) in cfg.options["blacklist"]:
+    if cfg.options.get("use_blacklist", False):
+        if str(message.author.id) in cfg.options.get("blacklist", []):
             return False
 
-    if len(str(message.content)) > 1999:
+    if len(str(message.content)) > cfg.options.get("message_limit", len(str(message.content))):
         return False
 
     return True
