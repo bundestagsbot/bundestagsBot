8a7b9be5bcc729c8c431a57e588b3601a5f6c263
Yannick <gross.fighterwar1@gmail.com>
2019-08-17 12:16:56 +0200

    added read function that does not handle errors. use with caution

--- bt_utils/handleJson.py
+++ bt_utils/handleJson.py
@@ -22,14 +22,29 @@ def readjson(rel_path, debug=True):
     if not BASE_PATH:
         SHL.output(f"Please set the BASE_PATH variable before.")
         return
-    rel_path = os.path.join(os.path.dirname(BASE_PATH), rel_path)
+    path = os.path.join(os.path.dirname(BASE_PATH), rel_path)
     try:
-        with open(rel_path, 'r') as c:
+        with open(path, 'r') as c:
             data = json.load(c)
     except FileNotFoundError:
-        if debug: SHL.output(f"{rel_path} not found.")
+        if debug: SHL.output(f"{path} not found.")
         return
     except:
         if debug: SHL.output(f"Invalid JSON")
         return
+    return data
+
+
+def read_json_raw(rel_path):
+    """
+    Warning: This function does not handle errors!
+    :param rel_path: relative path which get combined with BASE_PATH
+    :return: dict or None
+    """
+    if not BASE_PATH:
+        SHL.output(f"Please set the BASE_PATH variable before.")
+        return
+    rel_path = os.path.join(os.path.dirname(BASE_PATH), rel_path)
+    with open(rel_path, 'r') as c:
+        data = json.load(c)
     return data
