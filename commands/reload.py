dc1787169268cd93f247a862579e016011ca78b3
loknop <test@test.com>
2019-08-17 11:57:58 +0200

    new reload command to reload config

---
+++ commands/reload.py
@@ -0,0 +1,16 @@
+from bt_utils.console import Console
+from bt_utils.config import cfg
+SHL = Console('BundestagsBot reload')
+
+settings = {
+    'name': 'reload',
+    'channels': ['team1'],
+    'mod': True
+}
+
+async def main(client, message, params):
+    files_failed = cfg.reload()
+    if files_failed == 0:
+        await message.channel.send(content='All files reloaded')
+    else:
+        await message.channel.send(content=f'Failed to reload {files_failed} file(s)')
