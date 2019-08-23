35e3ba43be17467c5787831e929a840d0ed03661
MarkG <MarkGeiger@posteo.de>
2019-08-23 18:56:41 +0200

    fixed problem with user mentions in reactions.py

--- commands/reactions.py
+++ commands/reactions.py
@@ -13,22 +13,21 @@ settings = {
 
 async def main(client, message, params):
     users = DB.get_all_users()
-    params = str(message.content).split(' ')
-    if len(params) > 1:
-        user_id = params[1]
-        if len(user_id) > 2:
-            user_id = user_id[2:-1]
-            for user in users:
-                if user_id == str(user[0]):
-                    user_name = user[1]
-                    content = 'Reaktionen zu Nachrichten von ' + user_name + '\n'
-                    # skip name and id
-                    i = 2
-                    for role in cfg.options["roles"].items():
-                        if role[1] in cfg.options["roles_show"]:
-                            if user[i] > 0:
-                                content += "" + role[1] + ": " + str(user[i]) + "\n"
-                            i = i + 1
-                    await message.channel.send(content=message.author.mention + content)
-                    return
+    if len(message.mentions) == 0:
+        await message.channel.send(content=message.author.mention + 'Bitte einen Nutzer mit angeben')
+        return
+
+    for user in users:
+        if message.mentions[0].id == user[0]:
+            user_name = user[1]
+            content = 'Reaktionen zu Nachrichten von ' + user_name + '\n'
+            # skip name and id
+            i = 2
+            for role in cfg.options["roles"].items():
+                if role[1] in cfg.options["roles_show"]:
+                    if user[i] > 0:
+                        content += "" + role[1] + ": " + str(user[i]) + "\n"
+                    i = i + 1
+            await message.channel.send(content=message.author.mention + content)
+        return
     await message.channel.send(content=message.author.mention + 'Der Benutzer existiert nicht')
