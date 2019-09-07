a184df2837f1232eac08584a562a64d073570965
Yannick <zaanposni@users.noreply.github.com>
2019-09-07 16:04:18 +0200

    create or reset files on startup

--- others/reset_temps.py
+++ others/reset_temps.py
@@ -1,14 +1,36 @@
 from bt_utils.console import Console
 from bt_utils.config import cfg
 from bt_utils import handleJson
+import os
 SHL = Console("Reset")
 
 
+def reset_file(path, data):
+    try:
+        handleJson.saveasjson(path, data)
+    except FileNotFoundError:
+        SHL.output("File not found. Creating it.")
+        file_dir = os.path.join(handleJson.BASE_PATH, os.path.dirname(path))
+        if not os.path.exists(file_dir):
+            os.makedirs(file_dir)
+        handleJson.saveasjson(path, data)
+
+
 def reset():
-    SHL.output("Resetting temp files.")
-    # challenge json
-    data = {"arena_status": 0, "participants": []}
-    handleJson.saveasjson("content/challenge.json", data)
+    content = os.path.join(handleJson.BASE_PATH, "content")
+    if not os.path.exists(content):
+        SHL.output("Creating content folder.")
+        os.makedirs(content)
+
+        SHL.output("Resetting challenge.json")
+        reset_file("content/challenge.json", {"arena_status": 0, "participants": []})
+        SHL.output("Resetting submits.json")
+        reset_file("content/submits.json", {"latestID": 0})
+        SHL.output("Resetting subs.json")
+        reset_file("content/subs.json", {"unsubs": []})
+        SHL.output("Resetting surveys.json")
+        reset_file("content/surveys.json", {"latestID": 0})
 
     # cache file
-    handleJson.saveasjson(cfg.options.get("path_to_temp_cache", "temp/") + "cache.json", {})
+    SHL.output("Resetting cache file.")
+    reset_file(cfg.options.get("path_to_temp_cache", "temp/") + "cache.json", {})
