83ece9f0c36b44961d60d19b9b261c01589ca1f4
Yannick <zaanposni@users.noreply.github.com>
2019-09-08 18:13:30 +0200

    on reload update db structure of reaction stats

--- commands/reload.py
+++ commands/reload.py
@@ -1,7 +1,9 @@
 from bt_utils.console import Console
 from bt_utils.config import cfg
 from bt_utils.embed_templates import SuccessEmbed, WarningEmbed
+from bt_utils.handle_sqlite import DatabaseHandler
 SHL = Console('BundestagsBot Reload')
+DB = DatabaseHandler()
 
 settings = {
     'name': 'reload',
@@ -16,4 +18,13 @@ async def main(client, message, params):
         embed = SuccessEmbed('Success', 'All files reloaded')
     else:
         embed = WarningEmbed('Reloading failed', f'Failed to reload {files_failed} file(s)')
+
+    roles = cfg.options["roles_stats"].values()
+
+    # creates basic table structures if not already present
+    DB.create_structure(roles)
+
+    # updates table structure, e.g. if a new role has been added
+    DB.update_columns(roles)
+
     await message.channel.send(embed=embed)
