bcdef77e76f4cb12f57625fc50d34274afd8c255
loknop <loknop@users.noreply.github.com>
2019-09-29 11:24:43 +0200

    Rewwrote answer.py and added an option to provide multiple answers

--- src/commands/answer.py
+++ src/commands/answer.py
@@ -2,7 +2,7 @@ from bt_utils.console import Console
 from bt_utils.config import cfg
 from bt_utils.embed_templates import SuccessEmbed, NoticeEmbed, ErrorEmbed
 from bt_utils import handleJson
-
+from bt_utils.custom_exceptions import *
 SHL = Console("BundestagsBot Answer")
 
 settings = {
@@ -27,43 +27,51 @@ async def main(client, message, params):
         await message.channel.send(embed=error)
         return
     params = str(message.content).split(' ')
-    if subscribed(unsubs, message.author.id):
-        if len(params) == 3:
-            survey_id = params[1][1:]
-            if survey_id.isdigit():
-                if survey_id_is_valid(data, survey_id):
-                    survey_data = data[survey_id]
-                    if message.author.id not in survey_data['voted']:
-                        if int(str(params[2]).lower().strip()) in range(1, int(survey_data['answers']) + 1):
-                            vote(message.author.id, survey_id, params[2].lower().strip())
-                            success.description = f'Danke für deine Antwort!\n' \
-                                                  f'Du kannst die Ergebnisse mit\n' \
-                                                  f'{cfg.options["invoke_normal"]}result #{survey_id} sehen.'
-                            await message.channel.send(embed=success)
-                        else:
-                            error.description = f"Invalid answer.\nPossible answers: 1-{survey_data['answers']}"
-                            await message.channel.send(embed=error)
-                    else:
-                        error.description = "You cannot vote twice."
-                        await message.channel.send(embed=error)
-                else:
-                    error.description = f"#{survey_id} could not be assigned to a survey."
-                    await message.channel.send(embed=error)
-            else:
-                error.description = f"{survey_id} is an invalid ID."
-                await message.channel.send(embed=error)
-        else:
-            error.description = f"Invalid syntax.\nPlease use {cfg.options['invoke_normal']}answer #survey_id Answer"
-            await message.channel.send(embed=error)
-    else:
-        error.description = f"You have to be subscribed to participate. Use {cfg.options['invoke_normal']}sub True"
-        await message.channel.send(embed=error)
+    embed = error
+    try:
+        if not subscribed(unsubs, message.author.id):
+            raise UserNotSubscribedException()
+        if len(params) < 3:
+            raise CommandSyntaxException()
+        survey_id = params[1][1:]
+        if not survey_id.isdigit():
+            raise InvalidSurveyIdException(survey_id)
+        if not survey_id_is_valid(data, survey_id):
+            raise InvalidSurveyIdException(survey_id)
+        survey_data = data[survey_id]
+        if message.author.id in survey_data['voted']:
+            raise AlreadyVotedException()
+        answers = [e.lower().strip() for e in params[2:]]
+        invalid_answers = check_answers(answers, int(survey_data['answers']))
+        if invalid_answers:
+            raise AnswerNotFoundException(invalid_answers, survey_data['answers'])
+        vote(message.author.id, survey_id, answers)
+        embed = success
+        embed.description = f'Danke für deine Teilnahme!\n' \
+                            f'Du kannst die Ergebnisse mit\n' \
+                            f'{cfg.options["invoke_normal"]}result #{survey_id} sehen.'
+
+    except UserNotSubscribedException as e:
+        embed.description = f"You have to be subscribed to participate. Use {cfg.options['invoke_normal']}sub True"
+    except AnswerNotFoundException as e:
+        embed.description = f"Invalid answers: {e.invalid_answers}\nPossible answers: 1-{e.max_index}"
+    except AlreadyVotedException as e:
+        embed.description = "You cannot vote twice."
+    except SurveyNotFoundException as e:
+        embed.description = f"#{e.survey_id} could not be assigned to a survey."
+    except InvalidSurveyIdException as e:
+        embed.description = f"{e.survey_id} is an invalid ID."
+    except CommandSyntaxException as e:
+        embed.description = f"Invalid syntax.\nPlease use {cfg.options['invoke_normal']}answer #survey_id Answer"
+    finally:
+        await message.channel.send(embed=embed)
 
 
-def vote(user_id, survey_id, answer):
+def vote(user_id, survey_id, answers):
     data = handleJson.read_json_raw(path_to_surveys)
     data[survey_id]['voted'].append(user_id)
-    data[survey_id]['results'][answer] += 1
+    for answer in answers:
+        data[survey_id]['results'][answer] += 1
     handleJson.saveasjson(path_to_surveys, data)
 
 
@@ -73,3 +81,14 @@ def survey_id_is_valid(data, survey_id):
 
 def subscribed(data, user_id):
     return user_id not in data['unsubs']
+
+
+def check_answers(answers, max_ind):
+    invalid = []
+    for answer in answers:
+        try:
+            if int(answer) not in range(1, int(max_ind) + 1):
+                invalid.append(answer)
+        except ValueError:
+            invalid.append(answer)
+    return invalid
