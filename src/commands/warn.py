3bb2ae4c16ab2ec5bdffab4c11af7ee9feb575f6
loknop <loknop@users.noreply.github.com>
2019-12-24 03:14:02 +0100

    tidied up the code a bit and added the possibility to warn multiple users at once

--- src/commands/warn.py
+++ src/commands/warn.py
@@ -1,4 +1,5 @@
 from discord.utils import get
+from discord import HTTPException
 
 from bt_utils.console import Console
 from bt_utils.embed_templates import SuccessEmbed, WarningEmbed, NoticeEmbed, InfoEmbed
@@ -13,40 +14,37 @@ settings = {
 
 
 async def main(client, message, params):
-    badbois = str(message.content)[5:].strip()
-    badboi = None
-    for member in client.get_all_members():
-        if member.mention == badbois:
-            badboi = member
-            break
-    if not badboi:
-        embed = WarningEmbed(title="Warn", description=f"Could not find '{str(message.content)[5:].strip()}'")
-        await message.channel.send(embed=embed)
-        return
-    warned = get(message.guild.roles, id=cfg.options.get("warn_role_id", 0)) in badboi.roles
-    if not warned:
-        try:
-            punishrole = get(message.guild.roles, id=cfg.options.get("warn_role_id", 0))
-            await badboi.add_roles(punishrole)
-        except:
-            if cfg.options.get("warn_role_id", 0):
-                except_embed = WarningEmbed(title="Role-assigment failed",
-                                            description=f"Failed to assign role {cfg.options.get('warn_role_id', 0)}!")
-            else:
-                except_embed = WarningEmbed(title="Role-assigment failed",
-                                            description=f"No `warn_role_id` defined!")
-            await message.channel.send(embed=except_embed)
-            return
-        return_embed = SuccessEmbed(title="Warn", description="Warned " + badboi.mention)
-        try:
-            badboi_embed = InfoEmbed(title="Warn",
-                                     description=f"You were warned by {message.author.display_name}\n"
-                                                 f"Please read our server rules.\n"
-                                                 f"If you do not understand this message, please ask a moderator.")
-            await badboi.send(embed=badboi_embed)
-        except:  # if users privacy settings do not allow dm
-            pass
-        await message.channel.send(embed=return_embed)
-    else:
-        warned_embed = NoticeEmbed(title="Warn", description="User has already been warned once!")
-        await message.channel.send(embed=warned_embed)
+    to_warn = message.mentions
+    if not to_warn:
+        except_embed = WarningEmbed(title="No users supplied",
+                                   description=f"Usage: {cfg.options.get('invoke_normal')}warn @user")
+        await message.channel.send(embed=except_embed)
+
+    for user in to_warn:
+        warned = get(message.guild.roles, id=cfg.options.get("warn_role_id", 0)) in user.roles
+        if not warned:
+            try:
+                warn_role = get(message.guild.roles, id=cfg.options.get("warn_role_id", 0))
+                await user.add_roles(warn_role)
+            except HTTPException:
+                if cfg.options.get("warn_role_id", 0):
+                    except_embed = WarningEmbed(title="Role-assigment failed",
+                                                description=f"Failed to assign role {cfg.options.get('warn_role_id', 0)}!")
+                else:
+                    except_embed = WarningEmbed(title="Role-assigment failed",
+                                                description=f"No 'warn_role_id' defined!")
+                await message.channel.send(embed=except_embed)
+                return
+            return_embed = SuccessEmbed(title="Warn", description=f"Warned {user.name}")
+            try:
+                pm_embed = InfoEmbed(title="Warn",
+                                         description=f"You were warned by {message.author.display_name}\n"
+                                                     f"Please read our server rules.\n"
+                                                     f"If you do not understand this message, please ask a moderator.")
+                await user.send(embed=pm_embed)
+            except HTTPException:  # if users privacy settings do not allow dm
+                pass
+            await message.channel.send(embed=return_embed)
+        else:
+            warned_embed = NoticeEmbed(title="Warn", description=f"User {user.name} has already been warned once!")
+            await message.channel.send(embed=warned_embed)
